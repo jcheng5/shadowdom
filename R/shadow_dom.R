10da130e7294b214cdd8859c65ff93ed0f4568b4
Joe Cheng <joe@rstudio.com>
2020-04-03 17:49:11 -0700

    Add all_initial parameter, README

--- R/shadow_dom.R
+++ R/shadow_dom.R
@@ -6,17 +6,22 @@
 #' information (notably, Internet Explorer 11 is not supported).
 #'
 #' @inheritParams htmltools::tags
+#' @param all_initial Normally, inheritable styles (`background`, `color`,
+#'   `font`) are inherited across the shadow DOM boundary. Set `all_initial` to
+#'   `TRUE` to reset all styles to their initial (user agent provided) defaults
+#'   instead.
 #'
 #' @examples
 #' library(htmltools)
 #'
 #' ex <- div(
-#'   tags$style(HTML(
+#'   tags$style(HTML(c(
+#'     "body { font-family: sans-serif; }",
 #'     "p { color: blue; }"
-#'   )),
+#'   ))),
 #'   p("Here's a regular <p> tag; it's blue."),
 #'   shadow_dom(p("Here's a <p> tag in a shadow DOM; it's NOT blue.")),
-#'   shadow_dom(
+#'   shadow_dom(all_initial = TRUE,
 #'     tags$style(HTML("p { color: green; }")),
 #'     p("Here's a <p> tag in a shadow DOM with its own CSS; it's green.")
 #'   )
@@ -25,9 +30,12 @@
 #' print(ex, browse = TRUE)
 #'
 #' @export
-shadow_dom <- function(..., .noWS = NULL) {
+shadow_dom <- function(..., all_initial = FALSE, .noWS = NULL) {
   htmltools::attachDependencies(
-    htmltools::tag("x-with-shadow-dom", list(...), .noWS = .noWS),
+    htmltools::tag("x-with-shadow-dom", list(
+      if (all_initial) htmltools::tags$style(htmltools::HTML(":host {all: initial}")),
+      ...
+    ), .noWS = .noWS),
     shadow_dom_deps(),
     append = TRUE
   )
@@ -37,6 +45,7 @@ shadow_dom <- function(..., .noWS = NULL) {
 #'
 #' Use this function as a knitr render function.
 #'
+#' @inheritParams shadow_dom
 #' @param classes If provided, then the object to be printed by knitr must
 #'   inherit from at least one of these classes to be wrapped in [shadow_dom()];
 #'   objects that do not match will be forwarded to the `fallback` function.
@@ -51,13 +60,17 @@ shadow_dom <- function(..., .noWS = NULL) {
 #' }
 #'
 #' @export
-shadow_dom_knit_print <- function(classes = NULL, fallback = knitr::knit_print) {
+shadow_dom_knit_print <- function(classes = NULL,
+  fallback = knitr::knit_print,
+  all_initial = FALSE,
+  .noWS = NULL) {
+
   force(classes)
   force(fallback)
 
   function(x, ...) {
     if (is.null(classes) || inherits(x, classes)) {
-      x <- shadow_dom(x)
+      x <- shadow_dom(x, all_initial = all_initial, .noWS = .noWS)
     }
     fallback(x, ...)
   }
